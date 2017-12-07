e4f7e7377825951208103292551a3efc7b95a01d
Ronak Shah <ronakshah2013@u.northwestern.edu>
2017-12-07 17:53:28 -0600

    Improved workflow of SQL queries

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var inquirer = require('inquirer');
 // Connect to MySQL DB, use Env Vars so it works well between machines.
 var connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root', //To DO, use env vars
+    user: 'root', //To Do, use env vars
     password: 'mikesucks', //To Do, use Env vars
     // database: 'bamazon' 
 });
@@ -13,7 +13,6 @@ var connection = mysql.createConnection({
 connection.connect(function (err, res) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
-    // promptUser();
 });
 // Select the appropriate DB to use
 connection.query('USE bamazon', function (err, res) {
@@ -25,25 +24,69 @@ connection.query('SELECT * FROM products', function (err, res) {
     if (err) throw err;
     console.log(res);
 });
-promptUser()
 
-connection.end();
-function promptUser() {
+initialUserPrompt()
+
+function initialUserPrompt() {
     inquirer
-    .prompt([
-        {
-            type: 'confirm',
-            message: "Would you like to buy something?",
-            name: "userResponse"
-        }
-    ])
-    .then(function (inquirerResponse) {
-        if ('inquirerResponse.userResponse' == "Yes") {
-            console.log("He said yes!");
-        }
-        else {
-            console.log("He said no!");
-        };  
-    });
+        .prompt([
+            {
+                type: 'list',
+                message: "Would you like to buy something?",
+                choices: ["Yes", "No"],
+                name: "userResponse"
+            }
+        ])
+        .then(function (inquirerResponse) {
+            console.log(inquirerResponse.userResponse);
+            if (inquirerResponse.userResponse == "Yes") {
+                secondPrompt();
+            }
+            else {
+                console.log("He said no!");
+            };
+        });
 };
+
+
+function secondPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: 'prompt',
+                name: 'idResponse',
+                message: "What is the ID of the product you want to buy?"
+            },
+            {
+                type: 'prompt',
+                name: 'quantResponse',
+                message: 'How many units do you want to buy?'
+            }
+        ])
+        .then(function (inquirerResponse) {
+            connection.query('SELECT * FROM products WHERE item_id=?', [inquirerResponse.idResponse], function (err, res) {
+                if (err) throw "It's all broken :( ";
+                quantInStock = res[0].stock_quantity;
+                checkStuff(quantInStock, inquirerResponse);
+            });
+        });
+};
+
+function checkStuff(quant, inquirerResponse) {
+    if (quant < inquirerResponse.quantResponse) {
+        console.log("Sorry, you want to buy more than we have. Sadness");
+        console.log("Buy something else, we need money. We are a small business.");
+        secondPrompt();
+    }
+    else {
+        quant =- inquirerResponse.quantResponse;
+        connection.query('UPDATE stock_quantity WHERE item_id=?', [inquirerResponse.idResponse], function (err, res) {
+            if (err) throw "It's totally broken!";
+            else {
+                console.log("DONE!");
+            };
+        });
+    };
+}
+
 
