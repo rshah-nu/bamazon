dc15bcccf4cf6ffe9458cdbfa3a2586488e93859
Ronak Shah <ronakshah2013@u.northwestern.edu>
2017-12-08 15:52:15 -0600

    Prettified the output using a table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,32 +1,41 @@
 // Require NPM packages mysql and inquirer for db operations and user prompting
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var easyTable = require('easy-table');
 
 // Connect to MySQL DB, use Env Vars so it works well between machines.
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root', //To Do, use env vars
-    password: 'mikesucks', //To Do, use Env vars
-    // database: 'bamazon' 
+    password: '', //To Do, use Env vars
+    database: 'bamazon' 
 });
 // Create initial connection to db
 connection.connect(function (err, res) {
     if (err) throw err;
     console.log("Connected as id " + connection.threadId);
+    printDataToPage();
+    initialUserPrompt();
 });
-// Select the appropriate DB to use
-connection.query('USE bamazon', function (err, res) {
-    if (err) throw err;
-    console.log(res);
-});
-// Print all current products in DB to screen
-connection.query('SELECT * FROM products', function (err, res) {
-    if (err) throw err;
-    console.log(res);
-});
-
-initialUserPrompt()
-
+// Print all current products in DB to screen, with associated row data
+function printDataToPage() {
+    connection.query('SELECT * FROM products', function (err, res) {
+        var data = [];
+        if (err) throw err;
+        var t = new easyTable
+        
+        res.forEach(function (product) {
+            t.cell('Product ID', product.item_id)
+            t.cell('Description', product.product_name)
+            t.cell('Department', product.department_name)
+            t.cell('Price', product.price)
+            t.cell('Quantity in Stock', product.stock_quantity)
+            t.newRow()
+        });
+       console.log(t.toString())
+    });
+};
+// Prompts the user to take a DB action
 function initialUserPrompt() {
     inquirer
         .prompt([
@@ -43,7 +52,8 @@ function initialUserPrompt() {
                 secondPrompt();
             }
             else {
-                console.log("He said no!");
+                console.log("Goodbye! Next time, spend some money. We have to keep the lights on.");
+                connection.end();
             };
         });
 };
@@ -54,36 +64,38 @@ function secondPrompt() {
         .prompt([
             {
                 type: 'prompt',
-                name: 'idResponse',
+                name: 'userRequestedID',
                 message: "What is the ID of the product you want to buy?"
             },
             {
                 type: 'prompt',
-                name: 'quantResponse',
+                name: 'userRequestedQuantity',
                 message: 'How many units do you want to buy?'
             }
         ])
         .then(function (inquirerResponse) {
-            connection.query('SELECT * FROM products WHERE item_id=?', [inquirerResponse.idResponse], function (err, res) {
-                if (err) throw "It's all broken :( ";
-                quantInStock = res[0].stock_quantity;
-                checkStuff(quantInStock, inquirerResponse);
+            connection.query('SELECT * FROM products WHERE item_id=?', [inquirerResponse.userRequestedID], function (err, res) {
+                if (err) throw "Sorry, that item ID is not valid! Try harder next time!" + err;
+                // quantInStock = res[0].stock_quantity;
+                verifyStock(res, inquirerResponse);
             });
         });
 };
 
-function checkStuff(quant, inquirerResponse) {
-    if (quant < inquirerResponse.quantResponse) {
+function verifyStock(res, inquirerResponse) {
+    if (res[0].stock_quantity < inquirerResponse.userRequestedQuantity) {
         console.log("Sorry, you want to buy more than we have. Sadness");
         console.log("Buy something else, we need money. We are a small business.");
         secondPrompt();
     }
     else {
-        quant = quant - inquirerResponse.quantResponse;
-        connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [quant, inquirerResponse.idResponse], function (err, res) {
+        var newQuantity = res[0].stock_quantity - inquirerResponse.userRequestedQuantity;
+        connection.query('UPDATE products SET stock_quantity=? WHERE item_id=?', [newQuantity, inquirerResponse.userRequestedID], function (err, res) {
             if (err) throw err;
             else {
-                console.log("DONE!");
+                console.log("Thank you for your order! Have a nice day! Your credit card has been debited automatically, Michael and/or Peter");
+                printDataToPage();
+                connection.end();
             };
         });
     };
